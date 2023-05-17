4703cf038c002a455f7d940d27c448236b3a7b30
ARandomProgrammer <luis.henriques@estudante.ufscar.br>
2023-05-17 15:55:52 -0300

    new things

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
   import "../app.css";
-  import "@fontsource/montserrat";
+  import "@fontsource/montserrat/700.css";
+  import "@fontsource/montserrat/600.css";
+  import "@fontsource/montserrat/400.css";
   import "@fontsource/mulish";
     import Navbar from "../lib/components/Navbar.svelte";
 </script>
@@ -12,9 +14,13 @@
 
 <style lang="postcss">
     :global(button){
-        @apply px-6 py-3 font-sans rounded-full;
+        @apply px-6 py-3 font-sans rounded-full outline-none;
     }
     :global(button.primary) {
         @apply bg-primary;
+    }
+
+    :global(button.outline) {
+        @apply border-white text-white border-[1px];
     }
 </style>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="bg-black text-white pt-40">
+    <div class="flex items-center max-x-screen-xl mx-auto space-x-24">
+        <div class="flex flex-col w-1/2">
+            <div class="flex flex-col space-y-4">
+                <h1 class="text-5xl font-heading font-bold">Reach more people in real time</h1>
+                <p>
+                    The Screeno ecosystem is designed to help you generate profit. Set up complete sales and marketing funnels with ease using the Screeno
+                </p>
+            </div>
+            <div class="flex space-x-3 mt-12">
+                <button class="primary w-1/3">
+                    Start Free Trial
+                </button>
+                <button class="outline">
+                    Login
+                </button>
+            </div>
+        </div>
+        <div class="relative w-1/2">
+            <img src="/images/her-image-desktop.png" alt="Hero" class="w-full h-full">
+            <img src="/images/her-image-mobile.png" alt="Hero" class="absolute -bottom-4 left-3 ">
+        </div>
+        <div class="flex space-x-24">0
+            <img src="/images/apple-tv.png" alt="Apple TV">
+        </div>
+    </div>
+</section>
